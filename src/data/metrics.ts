ca0e967f7d1df84a2ccd2682fa9eda68b5bbcbca
LP-CREATIVE <133802681+LP-CREATIVE@users.noreply.github.com>
2025-05-12 10:38:17 -0500

    Update metrics.ts

--- src/data/metrics.ts
+++ src/data/metrics.ts
@@ -1,22 +1,22 @@
 export const metrics = [
   {
     title: "Total Reps",
-    value: "1,180",
+    value: "11,800",
     change: 0.05,
   },
   {
     title: "Incomplete Reps",
-    value: "15",
-    change: -0.01,
+    value: "1500",
+    change: -0.12,
   },
   {
     title: "Efficiency of Reps",
-    value: "87%",
+    value: "77%",
     change: 0.08,
   },
   {
     title: "Workout Score",
-    value: "90",
-    change: 1,
+    value: "81",
+    change: .10,
   },
 ];
