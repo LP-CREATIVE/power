329499be840061818af78ca03f5de176d6bcb94a
LP-CREATIVE <133802681+LP-CREATIVE@users.noreply.github.com>
2025-05-12 21:44:13 -0500

    Update index.tsx

--- src/components/chart-blocks/charts/average-tickets-created/index.tsx
+++ src/components/chart-blocks/charts/average-tickets-created/index.tsx
@@ -1,4 +1,4 @@
-"use client";
+ly"use client";
 
 import { useAtomValue } from "jotai";
 import { FilePlus2 } from "lucide-react";
@@ -28,18 +28,18 @@ export default function AverageTicketsCreated() {
   return (
     <section className="flex h-full flex-col gap-2">
       <div className="flex flex-wrap items-start justify-between gap-4">
-        <ChartTitle title="Average Tickets Created" icon={FilePlus2} />
+        <ChartTitle title="Weekly Performance Report" icon={FilePlus2} />
         <DatePickerWithRange className="" />
       </div>
       <div className="flex flex-wrap">
         <div className="my-4 flex w-52 shrink-0 flex-col justify-center gap-6">
           <MetricCard
-            title="Avg. Tickets Created"
+            title="Total Reps Tracked"
             value={avgCreated}
             color="#60C2FB"
           />
           <MetricCard
-            title="Avg. Tickets Resolved"
+            title="Effective Reps Completed"
             value={avgResolved}
             color="#3161F8"
           />
