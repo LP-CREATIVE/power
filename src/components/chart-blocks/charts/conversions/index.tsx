6096945fb069836a1ad892e1153960d1a62bfb85
LP-CREATIVE <133802681+LP-CREATIVE@users.noreply.github.com>
2025-05-12 21:36:19 -0500

    Update index.tsx

--- src/components/chart-blocks/charts/conversions/index.tsx
+++ src/components/chart-blocks/charts/conversions/index.tsx
@@ -7,7 +7,7 @@ import Chart from "./chart";
 export default function Convertions() {
   return (
     <section className="flex h-full flex-col gap-2">
-      <ChartTitle title="Conversions" icon={CirclePercent} />
+      <ChartTitle title="Intensity Score" icon={CirclePercent} />
       <Indicator />
       <div className="relative max-h-80 flex-grow">
         <Chart />
