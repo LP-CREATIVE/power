327f46ebd1ebf86505ce482ead78f9198ed132e0
LP-CREATIVE <133802681+LP-CREATIVE@users.noreply.github.com>
2025-05-15 16:33:06 -0500

    Update chart.tsx

--- src/components/chart-blocks/charts/conversions/chart.tsx
+++ src/components/chart-blocks/charts/conversions/chart.tsx
@@ -16,6 +16,17 @@ const spec: ICirclePackingChartSpec = {
   categoryField: "name",
   valueField: "value",
   drill: true,
+
+ color: {
+    type: "ordinal",
+    field: "name",
+    range: [
+      "hsl(0, 100%, 50%)",   // red
+      "hsl(0, 0%, 0%)",      // black
+      "hsl(0, 0%, 50%)",     // gray
+    ],
+  },
+  
   padding: 0,
   layoutPadding: 5,
   label: {
