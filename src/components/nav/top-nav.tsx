8716f2c8fe576ef685df93dcb95b4ee8e86d4587
LP-CREATIVE <133802681+LP-CREATIVE@users.noreply.github.com>
2025-05-17 15:35:24 -0500

    Update top-nav.tsx

--- src/components/nav/top-nav.tsx
+++ src/components/nav/top-nav.tsx
@@ -1,12 +1,13 @@
 "use client";
 
+import type { ReactNode } from "react";
 import Container from "../container";
 import { ThemeToggle } from "../theme-toggle";
 
-export default function TopNav({ title }: { title: string }) {
+export default function TopNav({ title }: { title: ReactNode }) {
   return (
     <Container className="flex h-16 items-center justify-between border-b border-border">
-      <h1 className="text-2xl font-medium">{title}</h1>
+      <div className="text-2xl font-medium">{title}</div>
       <ThemeToggle />
     </Container>
   );
