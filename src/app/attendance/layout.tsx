0a738496b459cd6853931a58048f45af3878ad06
LP-CREATIVE <133802681+LP-CREATIVE@users.noreply.github.com>
2025-05-17 21:36:59 -0500

    Create layout.tsx

---
+++ src/app/attendance/layout.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Container from "@/components/container";
+import { TopNav } from "@/components/nav";
+
+export default function AttendanceLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <TopNav
+        title={
+          <Image
+            src="/powerlogo.avif"
+            alt="Power Logo"
+            width={180}
+            height={180}
+            className="object-contain"
+          />
+        }
+      />
+      <main>
+        <Container>{children}</Container>
+      </main>
+    </>
+  );
+}
