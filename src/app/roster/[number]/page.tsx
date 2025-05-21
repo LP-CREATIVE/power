bce30a3c2bca174ab7fc5649c72db900c0f99ae8
LP-CREATIVE <133802681+LP-CREATIVE@users.noreply.github.com>
2025-05-20 19:22:41 -0500

    Update page.tsx

--- src/app/roster/[number]/page.tsx
+++ src/app/roster/[number]/page.tsx
@@ -1,8 +1,7 @@
-export default function Page(_: any) {
+export default function Page({}: { params: Record<string, string> }) {
   return (
     <div className="p-6">
       <h1 className="text-3xl font-bold">Welcome</h1>
     </div>
   );
 }
-
