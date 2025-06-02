d4802b8fc158ecf5189ed54bc67c573ddbe04682
LP-CREATIVE <133802681+LP-CREATIVE@users.noreply.github.com>
2025-06-02 08:24:04 -0500

    Update page.tsx

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,6 +1,6 @@
 // src/app/(dashboard)/page.tsx
 import {
-  AverageTicketsCreated,
+  AverageRepsCompleted,
   Conversions,
   CustomerSatisfication,
   Metrics,
@@ -15,7 +15,7 @@ export default function Home() {
 
       <div className="grid grid-cols-1 divide-y border-b border-border laptop:grid-cols-3 laptop:divide-x laptop:divide-y-0 laptop:divide-border">
         <Container className="py-4 laptop:col-span-2">
-          <AverageTicketsCreated />
+          <AverageRepsCompleted />
         </Container>
         <Container className="py-4 laptop:col-span-1">
           <Conversions />
