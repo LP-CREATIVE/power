4c220331b27e473189cd4c251a6959a86598a2d1
LP-CREATIVE <133802681+LP-CREATIVE@users.noreply.github.com>
2025-06-14 08:25:44 -0500

    Update page.tsx

--- src/app/roster/[id]/page.tsx
+++ src/app/roster/[id]/page.tsx
@@ -1,4 +1,7 @@
-export default function Page({ params }: { params: { id: string } }) {
-  return <h1>Dynamic Player: {params.id}</h1>;
+export default async function Page({ params }: { params: { id: string } }) {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">Player ID: {params.id}</h1>
+    </div>
+  );
 }
-
