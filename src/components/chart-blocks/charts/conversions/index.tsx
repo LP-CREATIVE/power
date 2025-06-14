d773a7a632b3f02c30ff06ce9f6a1094ab80d3c1
LP-CREATIVE <133802681+LP-CREATIVE@users.noreply.github.com>
2025-06-13 19:19:06 -0500

    fix typos in data and chart directories

--- src/components/chart-blocks/charts/conversions/index.tsx
+++ src/components/chart-blocks/charts/conversions/index.tsx
@@ -1,5 +1,5 @@
 import { CirclePercent } from "lucide-react";
-import { convertions } from "@/data/convertions";
+import { conversions } from "@/data/conversions";
 import { addThousandsSeparator } from "@/lib/utils";
 import ChartTitle from "../../components/chart-title";
 import Chart from "./chart";
@@ -21,7 +21,7 @@ function Indicator() {
     <div className="mt-3">
       <span className="mr-1 text-2xl font-medium">
         {addThousandsSeparator(
-          convertions.reduce((acc, curr) => acc + curr.value, 0),
+          conversions.reduce((acc, curr) => acc + curr.value, 0),
         )}
       </span>
       <span className="text-muted-foreground/60">Sales</span>
